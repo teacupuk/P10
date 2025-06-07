5fb77c839b47138ae9bb918dd1f8f3e4c34460fd
Thomas Pickup <thomas@duckshaw.co.uk>
2025-06-07 21:58:47 +0100

    Branding Update
    Added Logos for Favicons and MWA Logo and manifests

--- src/resources/views/components/application-head.blade.php
+++ src/resources/views/components/application-head.blade.php
@@ -5,6 +5,14 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     
+    <!-- Icons -->
+    <link rel="icon" type="image/png" href="{{ asset('favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}" />
+    <meta name="apple-mobile-web-app-title" content="The P10 Game" />
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}" />
+
     <!-- Scripts -->
     @vite(['resources/js/app.js'])
 
